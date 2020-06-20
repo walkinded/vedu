4bc655e1ab5df166510c0157ef67927cf067f4d2
Quadref <quadref.works@gmail.com>
2020-06-20 15:17:09 +0300

    project

---
+++ vedu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vedu
+//
+//  Created by Роман Лабунский on 20/06/2020.
+//  Copyright © 2020 Роман Лабунский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
